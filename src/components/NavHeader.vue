4a629927ef35cbfa798110b7ce675f366a5815d4
kntmr <beatkent@gmail.com>
2017-11-10 12:57:39 +0900

    header component

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="nav">
+    <div class="nav-left">
+      <h2>Vue Widget</h2>
+    </div>
+    <div class="nav-right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nav-header'
+}
+</script>
+
+<style scoped>
+.nav {
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+.nav-left {
+  width: 60%;
+}
+.nav-right {
+  width: 40%;
+  text-align: right;
+}
+</style>
