f0d71dec60ac779ccb2a27ffa582577847fdb9fd
kntmr <1657438+kntmr@users.noreply.github.com>
2020-09-14 23:28:23 +0900

    fix import

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
 
 Vue.config.productionTip = false
 
